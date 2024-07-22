3c90b2b59edfe96fe0386b599d5d1663afa77c4d
triton27 <na54heart@gmail.com>
2024-07-22 23:19:40 +0900

    add: 削除機能を追加

--- app.rb
+++ app.rb
@@ -8,19 +8,34 @@ before do
   @memos = JSON.load_file(JSON_PATH)
 end
 
+# トップ画面表示
 get '/' do
   @title = 'Top'
   erb :top
 end
 
+# 登録画面表示
 get '/new' do
   @title = 'New memo'
   erb :new
 end
 
-post '/' do
-  new_memo = { 'memo_id' => @memos.max_by { |m| m['memo_id'] }['memo_id'] + 1,
-               'memo_title' => params['memo_title'],
+# 登録処理
+post '/create' do
+  if @memos.empty?
+    memo_id = 1
+  else
+    memo_id = @memos.max_by { |m| m['memo_id'] }['memo_id'] + 1
+  end
+
+  if params['memo_title'].empty?
+    memo_title = "新しいメモ (#{memo_id})"
+  else
+    memo_title = params['memo_title']
+  end
+
+  new_memo = { 'memo_id' => memo_id,
+               'memo_title' => memo_title,
                'memo_description' => params['memo_description'] }
 
   @memos << new_memo
@@ -30,6 +45,7 @@ post '/' do
   erb :top
 end
 
+# 詳細画面表示
 get '/memos/:memo_id' do
   @title = 'Show memo'
 
@@ -43,6 +59,7 @@ get '/memos/:memo_id' do
   erb :show
 end
 
+# 編集画面表示
 get '/memos/:memo_id/edit' do
   @title = 'Edit memo'
 
@@ -56,7 +73,8 @@ get '/memos/:memo_id/edit' do
   erb :edit
 end
 
-patch '/memos/:memo_id/edit' do
+# 編集処理
+patch '/memos/:memo_id' do
   memo_id = params['memo_id'].to_i
 
   @memos.each do |m|
@@ -70,4 +88,14 @@ patch '/memos/:memo_id/edit' do
   File.open(JSON_PATH, 'w') { |file| file.write(JSON.pretty_generate(@memos)) }
 
   redirect "/memos/#{memo_id}"
+end
+
+# 削除処理
+delete '/memos/:memo_id' do
+  memo_id = params['memo_id'].to_i
+  @memos.delete_if { |m| m['memo_id'] == memo_id }
+
+  File.open(JSON_PATH, 'w') { |file| file.write(JSON.pretty_generate(@memos)) }
+
+  redirect '/'
 end
